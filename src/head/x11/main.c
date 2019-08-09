b65039786f735413e0138ee3bbb9d6fc8827c4fa
Nick Volpison <volpison@gmail.com>
2019-08-09 01:47:04 -0300

    update

--- src/head/x11/main.c
+++ src/head/x11/main.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2013 Nick Volpison
- * <volpison@mail.com>
+ * <volpison@gmail.com>
  *
  * Usage of the works is permitted provided that this instrument is retained
  * with the works, so that any entity that uses the works is notified of this
